d0f4bea024ae9a12f5824564d73f3761d5fbe99b
M4ttH3W711 <matthew711.uni@gmail.com>
2025-05-14 11:39:45 -0600

    Clase 2

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
 console.log("Hola");
+//En JS tenemos las fuenciones de tipo flecha
+//Utilizaresmos fetch que es un método para consumir una API
+
+//Paso 1: Función flecha
+const consumirApi = () => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+    //Convertimos la respuesta a tipo "JSON"
+    .then((response) => response.json())
+    //Los datos los vamos a amandar a la consola:
+    .then((data) => console.log(data))
+    //Descubrir qué hacer en caso de que no me corresponda
+    .catch((error) => console.log(error));
+};
+
+consumirApi();
+
+// TAREA EN INGLÉS
+// Consumir otra API y mostrar la data en consola como acá arriba
